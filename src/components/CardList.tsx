cacb5131b71243d50a05bce07b96e35efd330a03
Ryan Alencar <alencar.ryan08@aceproject.space>
2022-07-19 16:10:09 -0300

    feat: card list component

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -17,16 +17,31 @@ interface CardsProps {
 
 export function CardList({ cards }: CardsProps): JSX.Element {
   // TODO MODAL USEDISCLOSURE
+  const { isOpen, onClose } = useDisclosure();
+  const [selectedImage, setSelectedImage] = useState('');
 
   // TODO SELECTED IMAGE URL STATE
 
   // TODO FUNCTION HANDLE VIEW IMAGE
+  const handleViewImage = (imgUrl: string): void => {
+    setSelectedImage(imgUrl);
+  };
 
   return (
-    <>
+    <SimpleGrid columns={3} spacing="40px">
       {/* TODO CARD GRID */}
+      {cards.map(card => (
+        <>
+          <Card viewImage={() => handleViewImage(card.url)} data={card} />
+        </>
+      ))}
 
       {/* TODO MODALVIEWIMAGE */}
-    </>
+      <ModalViewImage
+        isOpen={isOpen}
+        onClose={onClose}
+        imgUrl={selectedImage}
+      />
+    </SimpleGrid>
   );
 }
